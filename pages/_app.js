6fc60437a4f68e7dcf28877feecb7c8b32d06994
Noel <noelsigmunczyk@users.noreply.github.com>
2021-01-08 17:31:17 +0100

    add chikra ui example

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,23 @@
 import 'typeface-dm-sans'
 import 'typeface-dm-serif-display'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+}
+
+const theme = extendTheme({ colors })
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { Button } from "@chakra-ui/react"
 
 export default function Home() {
   return (
@@ -6,6 +7,7 @@ export default function Home() {
       <Head>
         <title>Willkommen - Inselbühne Potsdam</title>
       </Head>
+      <Button colorScheme="blue">Button</Button>
     </div>
   )
 }

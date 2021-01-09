e8c68c354d0bf75d069343e05547dc8f34cd2a91
Noél Sigmunczyk <noelsigmunczyk@users.noreply.github.com>
2021-01-09 08:46:02 +0100

    run format

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,41 +1,41 @@
-import { extendTheme } from "@chakra-ui/react"
+import { extendTheme } from "@chakra-ui/react";
 
 const components = {
-    Heading: {
-        baseStyle: {
-            fontWeight: "normal",
-        },
-        variants: {
-            "uppercase": {
-                fontWeight: "bold",
-                textTransform: "uppercase",
-                letterSpacing: "0.18em",
-                fontFamily: "body",
-            }
-        }
+  Heading: {
+    baseStyle: {
+      fontWeight: "normal",
     },
-    Button: {
-        baseStyle: {
-            fontWeight: "normal",
-            borderRadius: ".5rem 0 .5rem 0",
-        }
-    }
-}
+    variants: {
+      uppercase: {
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        letterSpacing: "0.18em",
+        fontFamily: "body",
+      },
+    },
+  },
+  Button: {
+    baseStyle: {
+      fontWeight: "normal",
+      borderRadius: ".5rem 0 .5rem 0",
+    },
+  },
+};
 
 const colors = {
-    brand: {},
-    dark: "#0d0221",
-}
+  brand: {},
+  dark: "#0d0221",
+};
 
 const fonts = {
-    body: "'DM Sans', sans-serif",
-    heading: "'DM Serif Display', serif",
-}
+  body: "'DM Sans', sans-serif",
+  heading: "'DM Serif Display', serif",
+};
 
 const overrides = {
-    colors,
-    fonts,
-    components,
-}
+  colors,
+  fonts,
+  components,
+};
 
-export default extendTheme(overrides)
+export default extendTheme(overrides);

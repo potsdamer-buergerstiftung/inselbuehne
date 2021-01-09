50fa4cd1128d120edbdd762de635a7602d195cd1
Noél Sigmunczyk <noelsigmunczyk@users.noreply.github.com>
2021-01-09 09:07:10 +0100

    add bank address to page

--- pages/beitragen.tsx
+++ pages/beitragen.tsx
@@ -15,11 +15,32 @@ export default function Contribute() {
       </Title>
       <Box as="section" py="20">
         <Container centerContent textAlign="center">
-          <Text>
+          <Text mb="10">
             Auch ein kleiner Geldbeutel sorgt später für viele tolle und
             kostenlose Konzerte. Nach Deinem Beitrag kannst Du dir auch eine
             unserer Belohnungen abholen.
           </Text>
+          <Box
+            as="div"
+            background="green.500"
+            p="5"
+            boxShadow="base"
+            rounded="lg"
+            color="white"
+          >
+            <Text align="center" mb="5" fontSize="md">
+              Da sich unser Spendenmodul aktuell noch in der Entwicklung
+              befindet, bitten wir Dich, Deinen Beitrag zu überweisen.
+            </Text>
+
+            <Text align="center">
+              IBAN: DE93 1207 0000 0010 6633 00
+              <br />
+              BIC: DEUTDEBB160
+              <br />
+              Deutsche Bank Potsdam
+            </Text>
+          </Box>
         </Container>
       </Box>
     </Layout>

fbc2deaae47d3ecc7204882ce229203bf979abc1
Noél Sigmunczyk <noelsigmunczyk@users.noreply.github.com>
2021-01-09 07:59:41 +0100

    Add not found page

---
+++ pages/404.tsx
@@ -0,0 +1,17 @@
+import Layout from "../components/Layout";
+import Title from "../components/Title";
+import { Box, Container, Text } from "@chakra-ui/react";
+
+export default function PageNotFound() {
+  return (
+    <Layout title="Seite nicht gefunden" fluid>
+      <Title
+        title="Seite nicht gefunden"
+        heading="404"
+        color="red.100"
+      >
+        Es tut uns leid, aber Deine gesucht Seite konnte leider nicht gefunden werden.
+      </Title>
+    </Layout>
+  );
+}

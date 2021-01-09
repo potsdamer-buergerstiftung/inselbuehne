7d2923872b819cb349c2fe62006b996f7a203ecf
Noél Sigmunczyk <noelsigmunczyk@users.noreply.github.com>
2021-01-09 01:17:33 +0100

    Add donation section to layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,17 @@
 import Head from "next/head";
 import Header from "./Header";
 import Footer from "./Footer";
+import NextLink from "next/link";
+import {
+  Box,
+  Container,
+  Divider,
+  GridItem,
+  Heading,
+  Text,
+  Button,
+  Flex,
+} from "@chakra-ui/react";
 
 export default function Layout(props) {
   return (
@@ -14,6 +25,35 @@ export default function Layout(props) {
       </Head>
       <Header />
       {props.children}
+      <Box as="section" background="dark" color="gray.400">
+        <Container maxW="7xl">
+          <Box h="8" w="100%" />
+          <Flex align="center" justify="space-between">
+            <GridItem colSpan={8}>
+              <Heading as="h2" color="white" mb="2">
+                Lasst uns{" "}
+                <Box color="green.500" as="span">
+                  zusammen
+                </Box>{" "}
+                die Bühne beleben.
+              </Heading>
+              <Text>
+                Auch ein kleiner Beitrag sorgt später für viele kostenlose
+                Veranstaltungen.
+              </Text>
+            </GridItem>
+            <GridItem colSpan={4}>
+              <NextLink passHref href="/beitragen">
+                <Button colorScheme="green" size="lg" as="a">
+                  Jetzt spenden
+                </Button>
+              </NextLink>
+            </GridItem>
+          </Flex>
+          <Box h="8" w="100%" />
+          <Divider colorScheme="dark" />
+        </Container>
+      </Box>
       <Footer />
     </>
   );

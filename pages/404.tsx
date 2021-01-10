805d7b7150c7714bdeecfbfce2b23fc4669e402f
Noél Sigmunczyk <noelsigmunczyk@users.noreply.github.com>
2021-01-10 13:19:15 +0100

    make background color lighter

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,7 +5,7 @@ import { Box, Container, Text } from "@chakra-ui/react";
 export default function PageNotFound() {
   return (
     <Layout title="Seite nicht gefunden" fluid>
-      <Title title="Seite nicht gefunden" heading="404" color="red.100">
+      <Title title="Seite nicht gefunden" heading="404" color="red.50">
         Es tut uns leid, aber Deine gesuchte Seite konnte nicht gefunden werden.
       </Title>
     </Layout>

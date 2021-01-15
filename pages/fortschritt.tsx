7021c3df16c0ca1f9be6e3cec43cf631dae3c94e
Noel Sigmunczyk <noelsigmunczyk@users.noreply.github.com>
2021-01-15 18:28:04 +0100

    implement text on progress page

--- pages/fortschritt.tsx
+++ pages/fortschritt.tsx
@@ -13,10 +13,29 @@ export default function Progress() {
         Hier erfährst Du, was wir schon alles geschaft haben und was noch
         gemacht werden muss.
       </Title>
-      <Box as="section" py="20">
+      <Box as="section" pt="20" pb="10">
         <Container centerContent textAlign="center">
-          <Text>
-            Die aktuellen Blogbeiträge konnten leider nicht abgerufen werden.
+          <Text mb="8">
+            Das Projekt Inselbühne nimmt gerade erst Gestalt an. Klar ist, dass
+            es noch sehr viel zu tun gibt. Ob wir die Inselbühne tatsächlich
+            wiederbeleben können, steht noch in den Sternen. Es hängt zum Einen
+            daran, ob wir die Finanzierung für Spielbetrieb und bauliche
+            Ertüchtigung zusammen bekommen. Wir reden von ca. 50.000€ für den
+            Veranstaltungsbetrieb von Mai bis Oktober und ca. 80.000 € knapp
+            kalkuliert, für die provisorische Bespielbarkeit. Zum Anderen
+            benötigen wir viele Mitstreiter*innen, um die vielfältigen Aufgaben
+            dieses großen Projekts zu bewältigen.
+          </Text>
+          <Text mb="8">
+            Ohne ein gemeinsames finanzielles und persönliches Engagement der
+            Potsdamer Stadtgesellschaft wird eine Belebung der Inselbühne ein
+            Traum bleiben. Packen wir’s an – Du bist herzlich willkommen,
+            mitzumachen, mitzuwerben, mitzuspenden!
+          </Text>
+          <Text mb="8">
+            Auf dieser Seite wollen wir Dich regelmäßig über die Fortschritte
+            unseres Projekts informieren und Dich inspirieren, wo Du Dich
+            einklinken kannst.
           </Text>
         </Container>
       </Box>

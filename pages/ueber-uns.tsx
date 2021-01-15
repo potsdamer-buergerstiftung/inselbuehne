cedc32868fbf6fa6fdf788087ba08e352bef84a2
Noel Sigmunczyk <noelsigmunczyk@users.noreply.github.com>
2021-01-15 20:48:53 +0100

    add basic grid with placeholder image

--- pages/ueber-uns.tsx
+++ pages/ueber-uns.tsx
@@ -5,16 +5,55 @@ import {
   GridItem,
   Heading,
   Text,
-  Button,
-  Flex,
+  AspectRatio,
+  Link,
 } from "@chakra-ui/react";
 import Layout from "../components/Layout";
 import Title from "../components/Title";
 
+interface TeamMember {
+  name: string;
+  role: string;
+  details: string;
+}
+
+const team: TeamMember[] = [
+  {
+    name: "Marie-Luise Glahr",
+    role: "Vorsitzende",
+    details: "",
+  },
+];
+
 export default function AboutUst() {
   return (
     <Layout title="Über uns" fluid>
-      <Title title="Über uns" heading="Unsere Mission" color="gray.100"></Title>
+      <Title title="Über uns" heading="Unsere Mission"></Title>
+      <Box as="section" py="20">
+        <Container maxW="6xl">
+          <Heading as="h1" pb="10">
+            Unser Team
+          </Heading>
+          <Grid
+            columnGap={4}
+            rowGap={6}
+            templateColumns="repeat(12, 1fr)"
+            alignItems="center"
+          >
+            <GridItem colSpan={{ base: 12, md: 3 }}>
+              <Link>
+                <AspectRatio mb="5" ratio={1 / 1}>
+                  <Box background="gray.100"></Box>
+                </AspectRatio>
+              </Link>
+              <Text align="center" fontWeight={500}>
+                Marie-Luise Glahr
+              </Text>
+              <Text align="center">Vorsitzende</Text>
+            </GridItem>
+          </Grid>
+        </Container>
+      </Box>
     </Layout>
   );
 }

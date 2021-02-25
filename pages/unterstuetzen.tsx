06708f737e3b5bf040a5668fd994ceb65942385a
Nöl <65312133+linlogge@users.noreply.github.com>
2021-02-25 18:56:21 +0000

    set default donation amounts to 10, 20, 50, 100

--- pages/unterstuetzen.tsx
+++ pages/unterstuetzen.tsx
@@ -54,7 +54,7 @@ interface FormState {
   step: FormSteps;
 }
 
-const predefinedAmounts = [25, 50, 100, 200];
+const predefinedAmounts = [10, 20, 50, 100];
 
 interface FormContext {
   state: FormState;

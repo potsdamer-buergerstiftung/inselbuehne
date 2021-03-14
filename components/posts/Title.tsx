8c40b412c7adf8e2648ed1ced6d0a97d24b467a8
Nöl <65312133+linlogge@users.noreply.github.com>
2021-03-14 17:13:06 +0000

    handle case of no featured image

--- components/posts/Title.tsx
+++ components/posts/Title.tsx
@@ -12,10 +12,10 @@ const PostTitle: FC<PostTitleArgs> = ({ post }) => {
   const bg = useColorModeValue("green.100", "purple.900")
 
   return (
-    <Box bg={bg} py={{ base: 15, lg: 28 }} mb={-40}>
+    <Box bg={bg} py={post.featuredImage ? { base: 15, lg: 28 } : { base: "10rem", lg: "20rem" }} mb={-40}>
       <Container maxW="6xl">
-        <SimpleGrid columns={2} alignItems="center">
-          <Box mt={-24}>
+        <SimpleGrid columns={post.featuredImage ? 2 : 1} alignItems="center">
+          <Box mt={-24} textAlign={post.featuredImage ? null : "center"}>
             <Heading
               as="h6"
               variant="uppercase"
@@ -28,18 +28,20 @@ const PostTitle: FC<PostTitleArgs> = ({ post }) => {
             <Heading size="3xl" mb={5}>{post.title}</Heading>
             <Text>Ein Beitrag von {post.author.node.name}</Text>
           </Box>
-          <MotionBox>
-            <AspectRatio ratio={1 / 1}>
-              <Image
-                src={post.featuredImage.node.sourceUrl}
-                alt={`Bild von ${post.title}`}
-                objectFit="cover"
-                layout="fill"
-                quality={50}
-                sizes="300px"
-              />
-            </AspectRatio>
-          </MotionBox>
+          {post.featuredImage && (
+            <MotionBox>
+              <AspectRatio ratio={1 / 1}>
+                <Image
+                  src={post.featuredImage.node.sourceUrl}
+                  alt={`Bild von ${post.title}`}
+                  objectFit="cover"
+                  layout="fill"
+                  quality={50}
+                  sizes="300px"
+                />
+              </AspectRatio>
+            </MotionBox>
+          )}
         </SimpleGrid>
       </Container>
     </Box>

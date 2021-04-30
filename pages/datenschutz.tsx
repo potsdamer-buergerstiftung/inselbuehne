933999f3192a916985aa444b594696d37fece0d4
Nöl <65312133+linlogge@users.noreply.github.com>
2021-04-30 09:54:43 +0000

    add privacy page

---
+++ pages/datenschutz.tsx
@@ -0,0 +1,11 @@
+import { Heading } from "@chakra-ui/layout";
+import { PageTitle } from "@components/core";
+import { Default } from "@components/layouts"
+
+export default function Datenschutz() {
+  return (
+    <PageTitle title="Datenschutz"></PageTitle>
+  )
+}
+
+Datenschutz.Layout = Default;

4c0ec137a79e71bc7f86965acf60a885444b7b39
Nöl <65312133+linlogge@users.noreply.github.com>
2021-05-10 13:55:19 +0000

    remove console log

--- pages/api/events/index.ts
+++ pages/api/events/index.ts
@@ -2,7 +2,6 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { getEvents } from "@lib/api";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const events = await getEvents()
-  console.log(events)
+  const events = await getEvents();
   res.status(200).json(events);
 };

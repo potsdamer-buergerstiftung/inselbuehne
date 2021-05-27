bc4efe26fcc1fddaf1ebe240c62aabf0e166c6b9
Nöl <65312133+linlogge@users.noreply.github.com>
2021-05-27 11:45:58 +0000

    revoke api access in code

--- lib/api/events/categories.ts
+++ lib/api/events/categories.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { token } from ".";
 
 interface EventCategory {
   name: string;
@@ -7,7 +8,7 @@ interface EventCategory {
 export async function getEventCategories(): Promise<EventCategory[]> {
   return axios
     .get("https://www.eventbriteapi.com/v3/categories", {
-      headers: { Authorization: `Bearer ${"AS5DPUFLKCXWAJNVZRTQ"}` },
+      headers: { Authorization: `Bearer ${token}` },
       params: {
         locale: "de_DE",
       },
--- lib/api/events/events.ts
+++ lib/api/events/events.ts
@@ -1,22 +1,47 @@
 import axios from "axios";
+import { token } from ".";
 
 const organizationId = "528149989473";
 
 interface Event {
+  id: string;
   category: Record<any, any>;
+  status: string;
 }
 
-export async function getEvents(): Promise<Event[]> {
+interface GetEventOptions {
+  showPrivate?: boolean;
+}
+
+export async function getEvents(
+  options: GetEventOptions = {}
+): Promise<Event[]> {
+  const { showPrivate = false } = options;
+
   return axios
     .get(
       `https://www.eventbriteapi.com/v3/organizations/${organizationId}/events`,
       {
-        headers: { Authorization: `Bearer ${"FSXSKJNWHUJ4RMISG2IP"}` },
+        headers: { Authorization: `Bearer ${token}` },
         params: {
           expand: "category,subcategory",
           locale: "de_DE",
+          status: "live,started,ended,completed,canceled",
         },
       }
     )
+    .then((res) => res.data.events)
+    .then((res) => res.filter((event) => (showPrivate ? true : event.listed)));
+}
+
+export async function getEvent(id: string): Promise<Event> {
+  return axios
+    .get(`https://www.eventbriteapi.com/v3/events/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+      params: {
+        expand: "category,subcategory",
+        locale: "de_DE",
+      },
+    })
     .then((res) => res.data);
 }

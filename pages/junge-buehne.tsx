fb5bfdba3460a3831adcb1f4edd07a22ac5d3195
Nöl <65312133+linlogge@users.noreply.github.com>
2021-05-30 11:56:22 +0000

    add dark mode color

--- pages/junge-buehne.tsx
+++ pages/junge-buehne.tsx
@@ -2,6 +2,7 @@ import { Box, Container, Text } from "@chakra-ui/react";
 import { PageTitle } from "@components/core";
 import { Default } from "@components/layouts";
 import { MotionPageTransition } from "@components/motion";
+import { useColorModeValue as mode } from "@chakra-ui/color-mode";
 
 export default function JungeBuehne() {
   return (
@@ -9,12 +10,12 @@ export default function JungeBuehne() {
       <PageTitle
         title="Dürfen wir vorstellen: Die „Junge Bühne“"
         heading="Junge Bühne"
-        bg="blue.100"
+        bg={mode("blue.100", "blue.900")}
       >
         Auf dieser Seite werden wir demnächst Informationen und Formulare
         bereitstellen, um Euch die Anmeldung zu erleichtern.
       </PageTitle>
-      <Box as="section" py="20" bg="blue.100"></Box>
+      <Box as="section" py="20" bg={mode("blue.100", "blue.900")}></Box>
     </MotionPageTransition>
   );
 }

88d67cc286b362a0dfd04bfe46d951333f5a15c0
Nöl <65312133+linlogge@users.noreply.github.com>
2021-06-04 09:47:46 +0000

    add phone contact option

--- pages/kontakt.tsx
+++ pages/kontakt.tsx
@@ -9,8 +9,9 @@ import {
   Link,
   useColorModeValue,
   SimpleGrid,
+  Divider,
 } from "@chakra-ui/react";
-import { EmailIcon, InstagramIcon } from "@components/icons";
+import { EmailIcon, InstagramIcon, PhoneIcon } from "@components/icons";
 import { MotionPageTransition } from "@components/motion";
 import { InquiryForm } from "@components/contact";
 import axios from "axios";
@@ -25,12 +26,12 @@ const ContactPage = () => {
     <MotionPageTransition>
       <NextSeo title="Kontakt" />
       <PageTitle title="Wie können wir Dir helfen?" heading="Kontakt" bg={bg}>
-        Hast Du Fragen bezüglich der Inselbühne? Dann kontaktiere uns doch
-        einfach!
+        Hast Du Fragen bezüglich der Inselbühne oder möchtest Tickets bestellen?
+        Dann kontaktiere uns doch einfach!
       </PageTitle>
       <Box as="section" pb="20" bg={bg}>
-        <Container>
-          <SimpleGrid spacing={10} columns={2}>
+        <Container maxW="container.md">
+          <SimpleGrid spacing={10} columns={3}>
             <Box textAlign="center">
               <Box as="div" pb="5">
                 <EmailIcon boxSize={12} color="green.500" />
@@ -57,11 +58,24 @@ const ContactPage = () => {
                 </Link>
               </Text>
             </Box>
+            <Box textAlign="center">
+              <Box as="div" pb="5">
+                <PhoneIcon boxSize={12} color="green.500" />
+              </Box>
+              <Heading as="h6" variant="uppercase" fontSize="sm">
+                Telefon
+              </Heading>
+              <Text color={color}>
+                <Link href="tel:+4915739618032" isExternal>
+                  +49 157 39618032
+                </Link>
+              </Text>
+            </Box>
           </SimpleGrid>
         </Container>
       </Box>
-      <Box as="section" pb="20" bg={bg}>
-        <Container>
+      <Box as="section" pb="28" bg={bg}>
+        <Container maxW="container.md">
           <InquiryForm onSubmit={onSubmit} />
         </Container>
       </Box>

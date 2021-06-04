d7742f20813153d921f6f8d4c3e995d0dbfe1a92
Nöl <65312133+linlogge@users.noreply.github.com>
2021-06-04 09:47:07 +0000

    add phone icon

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -2,4 +2,5 @@ import DoneIcon from "./Done";
 import EmailIcon from "./Email";
 import ErrorIcon from "./Error";
 import InstagramIcon from "./Instagram";
-export { DoneIcon, EmailIcon, ErrorIcon, InstagramIcon };
+import PhoneIcon from "./Phone";
+export { DoneIcon, EmailIcon, ErrorIcon, InstagramIcon, PhoneIcon };

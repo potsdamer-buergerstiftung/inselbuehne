de5935047e994fe21a78f1349314a5279f41cb6c
Nöl <65312133+linlogge@users.noreply.github.com>
2021-06-04 16:49:34 +0000

    remove fill rule

--- components/icons/Phone.tsx
+++ components/icons/Phone.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 
 const PhoneIcon: FC<IconProps> = (props) => (
   <Icon viewBox="0 0 24 24" {...props}>
-    <g fill="none" fill-rule="evenodd">
+    <g fill="none">
       <path d="M0 0h24v24H0z"></path>
       <path
         d="M13.08 14.784l2.204-2.204a2 2 0 00.375-2.309l-.125-.25a2 2 0 01.374-2.308l2.733-2.733a.5.5 0 01.801.13l1.104 2.208a4.387 4.387 0 01-.822 5.065l-5.999 5.998a5.427 5.427 0 01-5.553 1.311l-2.415-.804a.5.5 0 01-.195-.828l2.65-2.652a2 2 0 012.31-.374l.25.125a2 2 0 002.308-.375z"
